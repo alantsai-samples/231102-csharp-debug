ccd0ecd1ce73e13bfab9f82ad9f57d3e60b0963a
Alan Tsai <alan@alantsai.net>
2023-11-02 18:36:13 +0800

    add demo files

---
+++ src/Console1/Demo03.cs
@@ -0,0 +1,17 @@
+public class Demo03
+{
+    public void Run()
+    {
+        bool exit = false;
+        var rand = new Random();
+        int num1 = 5;
+        int num2 = 5;
+
+        do
+        {
+            num1 = rand.Next(1, 11);
+            num2 = num1 + rand.Next(1, 51);
+
+        } while (exit == false);
+    }
+}
---
+++ src/Console1/Program.cs
@@ -0,0 +1,9 @@
+// var demo = new Demo01();
+
+
+var demo = new Demo02();
+
+//var demo = new Demo03();
+demo.Run();
+
+System.Console.WriteLine("console complete");
